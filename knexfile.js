093cdf0d96cf1d0da850b5b76c8eac98f72d4e74
Bruno Sales <engbrunosales@gmail.com>
2020-01-17 22:48:47 -0300

    Revert "Inserido configuração de Development e ajustado o directory das migrations e seeds - closed nº1"
    This reverts commit 7fa20780d5e422a0e5f6be56c5c186fb879d10c9.

--- knexfile.js
+++ knexfile.js
@@ -1,52 +1,30 @@
 /*
   CREDENCIAIS DO BANCO 
-  - development: banco de desenvolvimento
-  - test: banco de test
+  - test: banco de desenvolvimento
   - production: banco de produção
 */
 require('dotenv').config();
 
 module.exports = {
-  development: {
-    client: process.env.DB_CLIENT,
-    connection: {
-      host: process.env.DB_HOST,
-      database: 'gestorfinaceiro_dev',
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      charset: process.env.DB_CHARSET
-    },
-    seeds: {
-      directory: 'src/db/seeds/'
-    },
-    migrations: {
-      directory: 'src/db/migrations/'
-    }
-  },
   test: {
     client: process.env.DB_CLIENT,
     connection: {
       host: process.env.DB_HOST,
-      database: 'gestorfinaceiro_test',
+      database: process.env.DB_NAME,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       charset: process.env.DB_CHARSET
     },
-    seeds: {
-      directory: 'src/db/seeds/'
-    },
     migrations: {
-      directory: 'src/db/migrations/'
+      diretory: 'src/migrations',
+      tableName: 'migrations'
     }
   },
   production: {
     client: process.env.DB_CLIENT,
     connection: process.env.DB_URL + `?ssl=true`,
-    seeds: {
-      directory: __dirname + 'src/db/seeds/'
-    },
     migrations: {
-      directory: __dirname + 'src/db/migrations/'
+      diretory: __dirname + 'src/migrations'
     }
   }
 }
