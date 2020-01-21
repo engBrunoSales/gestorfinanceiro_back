402a20a0ae083c5dd23b939f663cee73303aee70
Jemima <contatojemima@gmail.com>
2020-01-21 13:57:22 -0300

    https://github.com/engBrunoSales/gestorfinanceiro_back/issues/9

--- src/app.js
+++ src/app.js
@@ -23,5 +23,21 @@ app.get('/', (req, res) => {
   res.status(200).send()
 })
 
+// middleware para tratamento de erros
+app.use((err, req, res, next) => {
+  const { name, message } = err
+  if (name === 'ValidationError') res.status(400).json({ error: message })
+  if (name === 'RecursoIndevidoError') res.status(403).json({ error: message })
+  else res.status(500).json({ name, message })
+  next(err)
+})
+
+// LOGGER MANUAL - para visualizar o que as querys tão retornando é só descomentar
+// app.db.on('query', (query) => {
+//   console.log({ sql: query.sql, bindings: query.bindings ? query.bindings.join(',') : '' })
+// }).on('query-response', (response) => {
+//   console.log(response)
+// })
+
 // exportando o objeto app
 module.exports = app
