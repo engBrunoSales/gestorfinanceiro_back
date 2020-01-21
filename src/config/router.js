c69b03af02e45c777da8830bf8c9a0b77f4c32b5
Jemima <contatojemima@gmail.com>
2020-01-21 13:59:52 -0300

    https://github.com/engBrunoSales/gestorfinanceiro_back/issues/10

---
+++ src/config/router.js
@@ -0,0 +1,16 @@
+/*
+  MÒDULO DE ROTAS (ROTEADOR):
+  notação: app.route('nomeDaRota').[metodo](app.pasta.arquivo.funcaoCorrespondente)
+*/
+
+const express = require('express')
+
+module.exports = app => {
+  // app.use('/auth', app.routes.auth)
+  const protectedRouter = express.Router()
+
+  protectedRouter.use('/users', app.routes.users)
+
+  // roteador rotegido VERSÂO 1
+  // app.use('/v1', app.config.passport.authenticate(), protectedRouter)
+}
