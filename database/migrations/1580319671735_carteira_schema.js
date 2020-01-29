1c5c2e429ffb628e7fb68b66d490439383852d9c
Bruno Sales <engbrunosales@gmail.com>
2020-01-29 15:41:00 -0300

    Migration: Carteiras criado
    Realizado a criação da migration para o model: Carteira

---
+++ database/migrations/1580319671735_carteira_schema.js
@@ -0,0 +1,23 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class CarteiraSchema extends Schema {
+  up () {
+    this.create('carteiras', (table) => {
+      table.increments()
+      table.string('nome', 100).notNullable()
+      table.integer('user_id').unsigned().notNullable().index('idx_user_id_carteira');
+      table.timestamps()
+
+      table.foreign('user_id').references('id').inTable('users')
+    })
+  }
+
+  down () {
+    this.drop('carteiras')
+  }
+}
+
+module.exports = CarteiraSchema
