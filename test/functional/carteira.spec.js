ae36a0c315fa601134edb1167a3a088ca4242f34
Bruno Sales <engbrunosales@gmail.com>
2020-01-29 22:20:52 -0300

    Criado o CRUD do model: Carteira com todos os testes

--- test/functional/carteira.spec.js
+++ test/functional/carteira.spec.js
@@ -7,22 +7,90 @@ const Carteira = use('App/Models/Carteira')
 trait('Test/ApiClient')
 trait('Auth/Client')
 
-test('Consulta de todas as carteiras de um usuário', async ({ client }) => {
-  assert.equal(0, 4)
+const URL_BASE = '/api/carteiras/'
+
+test('Consulta de todas as carteiras de um usuário', async ({ assert, client }) => {
+  const user = await Factory.model('App/Models/User').create()
+  const carteira = await user.carteiras().createMany([
+    {nome: 'Feverreiro 2020'},
+    {nome: 'Janeiro 2020'}
+  ])
+
+  const response = await client.get(URL_BASE)
+  								.loginVia(user, 'jwt')
+                  .end()
+  response.assertStatus(200)
+  response.assertJSONSubset([
+    {nome: 'Feverreiro 2020'},
+    {nome: 'Janeiro 2020'}
+  ])
 })
 
-test('Consulta de uma carteira especifica', async ({ client }) => {
-  assert.equal(0, 4)
+test('Consulta de uma carteira especifica', async ({ assert, client }) => {
+  const user = await Factory.model('App/Models/User').create()
+  const carteira = await user.carteiras().create({nome: 'Feverreiro 2020'})
+
+  const response = await client.get(URL_BASE+carteira.id)
+  								.loginVia(user, 'jwt')
+                  .end()
+  response.assertStatus(200)
+  response.assertJSONSubset([
+    {nome: 'Feverreiro 2020'}
+  ])
 })
 
-test('Criação de uma carteira', async ({ client }) => {
-  assert.equal(0, 4)
+test('Criação de uma carteira', async ({ assert, client }) => {
+  const user = await Factory.model('App/Models/User').create()
+  const data = {
+    nome: "Feverreiro 2020"
+  }
+
+  const response = await client.post(URL_BASE)
+                  .loginVia(user, 'jwt')
+                  .send(data)
+                  .end()
+  response.assertStatus(204)
 })
 
-test('Alteração de uma carteira', async ({ client }) => {
-  assert.equal(0, 4)
+test('Criação de uma carteira inválida', async ({ assert, client }) => {
+  const user = await Factory.model('App/Models/User').create()
+  const data = {
+    nome: 1
+  }
+
+  const response = await client.post(URL_BASE)
+                  .loginVia(user, 'jwt')
+                  .send(data)
+                  .end()
+  response.assertStatus(400)
+  response.assertJSONSubset([{
+    field: 'nome',
+    message: 'nome não é uma string válida',
+    validation: 'string'
+  }])
 })
 
-test('Deleção de uma carteira', async ({ client }) => {
-  assert.equal(0, 4)
+test('Alteração de uma carteira', async ({ assert, client }) => {
+  const user = await Factory.model('App/Models/User').create()
+  const carteira = await user.carteiras().create({nome: 'Feverreiro 2020'})
+  const data = {
+    nome: "Janeiro 2020"
+  }
+
+  const response = await client.put(URL_BASE+carteira.id)
+                  .loginVia(user, 'jwt')
+                  .send(data)
+                  .end()
+  response.assertStatus(204)
+})
+
+test('Deleção de uma carteira', async ({ assert, client }) => {
+  const user = await Factory.model('App/Models/User').create()
+  const carteira = await user.carteiras().create({nome: 'Feverreiro 2020'})
+
+  const response = await client.delete(URL_BASE+carteira.id)
+                  .loginVia(user, 'jwt')
+                  .end()
+  response.assertStatus(204)
+  assert.equal(await user.carteiras().getCount(), 0)
 })
